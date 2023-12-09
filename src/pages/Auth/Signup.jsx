65f293d318594ed4767df6a77dc1d57b1218deb5
Beongyoung Park <sergiopark10373@likelion.org>
2023-12-09 17:14:25 +0900

    :fire: Oauth 추가
    1. google 로그인
    2. 회원가입 간소화

--- src/pages/Auth/Signup.jsx
+++ src/pages/Auth/Signup.jsx
@@ -1,27 +1,122 @@
-import { Container, Typography } from "@mui/material";
-import SignupDone from "../../components/Signup/SignupDone.jsx";
-import SignupInfo from "../../components/Signup/SignupInfo.jsx";
-import SignupTerms from "../../components/Signup/SignupTerms.jsx";
-import SignupStepper from "../../components/Signup/SignupStepper.jsx";
+import { useState } from "react";
+import {
+  Button,
+  Container,
+  Typography,
+  Box,
+  TextField,
+  Stack,
+} from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios"; // Import Axios
 
-function Signup() {
-  const steps = ["1. 약관동의", "2. 회원정보 입력", "3. 회원가입 완료"];
+function SignUpPage() {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const navigate = useNavigate();
+
+  const onSignUpClick = async (e) => {
+    e.preventDefault(); // Prevent the default form submission
+
+    if (password !== confirmPassword) {
+      alert("비밀번호가 일치하지 않습니다.");
+      setPassword("");
+      setConfirmPassword("");
+      return;
+    }
+
+    try {
+      // Use Axios to make a POST request
+      const response = await axios.post("localhost:0880", {
+        userId: id,
+        password: password,
+      });
+
+      // Handle the response data, e.g., show a message based on success or failure
+      console.log(response.data);
+
+      // Redirect the user upon successful signup
+      if (response.status === 200) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.error("Error during signup:", error);
+    }
+  };
+
   return (
     <Container>
       <Typography
         variant={"h5"}
         fontWeight={"bold"}
-        sx={{ mt: 8, mb: 4, borderBottom: "1px solid black" }}
+        sx={{ mt: 8, borderBottom: "1px solid black" }}
       >
         회원가입
       </Typography>
-      <SignupStepper steps={steps}>
-        <SignupTerms />
-        <SignupInfo />
-        <SignupDone />
-      </SignupStepper>
+      <Box
+        sx={{
+          mt: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <form onSubmit={onSignUpClick}>
+          <Stack
+            justifyContent={"center"}
+            alignItems={"center"}
+            spacing={1}
+            width={"400px"}
+          >
+            <TextField
+              fullWidth
+              label="아이디"
+              variant="outlined"
+              placeholder="아이디를 입력하세요"
+              value={id}
+              onChange={(e) => setId(e.target.value)}
+            />
+
+            <TextField
+              fullWidth
+              label="비밀번호"
+              variant="outlined"
+              placeholder="비밀번호를 입력하세요"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <TextField
+              fullWidth
+              label="비밀번호 재입력"
+              variant="outlined"
+              placeholder="비밀번호를 다시 입력하세요"
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+
+            <Button variant="contained" type="submit" fullWidth>
+              회원가입
+            </Button>
+
+            <Box
+              display={"flex"}
+              width={"100%"}
+              justifyContent={"center"}
+              alignItems={"center"}
+            >
+              <Link to={"/auth/login"}>
+                <Button>로그인</Button>
+              </Link>
+            </Box>
+          </Stack>
+        </form>
+      </Box>
     </Container>
   );
 }
 
-export default Signup;
+export default SignUpPage;
