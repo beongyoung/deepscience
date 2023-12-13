02c223dd4d499d87e0b9633d5e25f7a9d6957aa0
Beongyoung Park <sergiopark10373@likelion.org>
2023-12-13 11:30:59 +0900

    :recycle: update

--- src/hooks/fetchNews.jsx
+++ src/hooks/fetchNews.jsx
@@ -3,13 +3,7 @@ const NEWS_API_KEY = import.meta.env.VITE_NEWS_API_KEY;
 export async function fetchNews() {
   try {
     const response = await fetch(
-      `https://newsapi.org/v2/top-headlines?country=kr&category=technology&apiKey=${NEWS_API_KEY}`,
-      {
-        headers: {
-          Upgrade: "HTTP/2.0",
-          Connection: "Upgrade",
-        },
-      }
+      `https://newsapi.org/v2/top-headlines?country=kr&category=technology&apiKey=${NEWS_API_KEY}`
     );
 
     if (!response.ok) {
