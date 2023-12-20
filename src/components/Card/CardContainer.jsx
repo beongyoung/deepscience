e291a3703ae8835b3a953a02f547a421890d2922
Beongyoung Park <sergiopark10373@likelion.org>
2023-12-20 20:55:26 +0900

    :recycle: Update News

--- src/components/Card/CardContainer.jsx
+++ src/components/Card/CardContainer.jsx
@@ -2,9 +2,6 @@ import styled from "styled-components";
 
 const CardContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 16px;
-  align-content: center;
   justify-content: center;
   justify-items: center;
   list-style: none;
