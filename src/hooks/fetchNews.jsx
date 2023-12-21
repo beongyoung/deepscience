f9d51de2713034dc87024c44f908a12f90a94a6b
Beongyoung Park <sergiopark10373@likelion.org>
2023-12-21 21:29:45 +0900

    :recycle: fetch news

--- src/hooks/fetchNews.jsx
+++ src/hooks/fetchNews.jsx
@@ -1,19 +1,16 @@
 import axios from "axios";
-const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
 const NEWS_API_KEY = import.meta.env.VITE_NEWS_API_KEY;
 
 export async function fetchNews() {
   try {
-    const response = await axios.get(
-      `${proxyurl}https://newsapi.org/v2/top-headlines`,
-      {
-        params: {
-          country: "kr",
-          category: "technology",
-          apiKey: NEWS_API_KEY,
-        },
-      }
-    );
+    const response = await axios.get("https://newsapi.org/v2/top-headlines", {
+      params: {
+        country: "kr",
+        category: "technology",
+        apiKey: NEWS_API_KEY,
+      },
+    });
 
     if (response.status !== 200) {
       throw new Error(`HTTP error! Status: ${response.status}`);
