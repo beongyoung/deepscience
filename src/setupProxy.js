18c70d836a9315604cc703b889922114cbb52475
Beongyoung Park <sergiopark10373@likelion.org>
2023-12-26 14:55:59 +0900

    :recycle: proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,39 @@
 import express from "express";
 import cors from "cors";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 const app = express();
 app.use(
   cors({ origin: ["https://localhost:5173", "https://deepscience.vercel.app"] })
 );
+app.use(
+  "/v1",
+  createProxyMiddleware({
+    target: "http://api.updatecome.com:8081/",
+    changeOrigin: true,
+    secure: false,
+    ws: true,
+  })
+);
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: "https://opendart.fss.or.kr/",
+    changeOrigin: true,
+    secure: false,
+    ws: true,
+    cors: true,
+  })
+);
+app.use(
+  "/v2",
+  createProxyMiddleware({
+    target: "https://newsapi.org/",
+    changeOrigin: true,
+    secure: false,
+    ws: true,
+    cors: true,
+  })
+);
+
+export default app;
