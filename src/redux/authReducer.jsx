ab8eb0dc6ef87699ac41bc3bef79c1db56a26d80
Beongyoung Park <sergiopark10373@likelion.org>
2023-12-28 01:51:48 +0900

    :recycle:

--- src/redux/authReducer.jsx
+++ src/redux/authReducer.jsx
@@ -16,7 +16,7 @@ const authReducer = (state = initialState, action) => {
         authUser: action.payload,
       };
     case "LOGOUT_USER":
-      return undefined;
+      return initialState;
     default:
       return state;
   }
