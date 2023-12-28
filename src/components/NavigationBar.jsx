7554653ca14f49cf762a73e236da41cd978077d8
Beongyoung Park <sergiopark10373@likelion.org>
2023-12-29 04:21:52 +0900

    :recycle: rendering the User

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -59,8 +59,14 @@ function NavigationBar() {
   const authUser = authUserString ? JSON.parse(authUserString) : null;
 
   useEffect(() => {
+    console.log("Inside useEffect");
     try {
+      if (authUser === null) {
+        console.log("Reloading...");
+        window.location.reload();
+      }
       if (authUser?.data && !showWelcomeAlert) {
+        console.log("Showing welcome alert");
         setShowWelcomeAlert(true);
         toast.info(`${authUser.data.name}님 환영합니다!`);
       }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import App from "./App.jsx";
 import { RecoilRoot } from "recoil";
 import "./App.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <RecoilRoot>
       <App />
