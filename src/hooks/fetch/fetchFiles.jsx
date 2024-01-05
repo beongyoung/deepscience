c3cd290022da57602bda29390718b46bfa4b97cb
Beongyoung Park <sergiopark10373@likelion.org>
2024-01-05 22:47:53 +0900

    :recycle: update

--- src/hooks/fetch/fetchFiles.jsx
+++ src/hooks/fetch/fetchFiles.jsx
@@ -3,15 +3,15 @@ import axios from "axios";
 const authToken =
   localStorage.getItem("authCode") || import.meta.env.VITE_SUCCESS_TOKEN;
 
-const getInstance = (id = 1) => {
+function getInstance(id = 1) {
   return axios.create({
     baseURL: `/v1/companies/${id}/quant-analysis`,
     headers: {
-      "Content-Type": "application/json",
+      "content-type": "multipart/form-data",
       "X-AUTH-TOKEN": authToken,
     },
   });
-};
+}
 
 const fetchFiles = {
   uploadPdf: async (id, formData) => {
