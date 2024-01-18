6570e415c3cc4d348e9f93439921753e437d3f4c
Beongyoung Park <sergiopark10373@likelion.org>
2024-01-18 15:28:24 +0900

    Update

--- vite.config.js
+++ vite.config.js
@@ -13,22 +13,26 @@ export default defineConfig({
         target: "https://api.updatecome.com:8081",
         changeOrigin: true,
         secure: false,
+        rewrite: (path) => path.replace(/^\/v1/, ""),
       },
       "/api": {
         target: "https://opendart.fss.or.kr",
         changeOrigin: true,
         secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
       "/v2/top-headlines": {
         target: "https://newsapi.org",
         changeOrigin: true,
         secure: false,
+        rewrite: (path) => path.replace(/^\/v2\/top-headlines/, ""),
       },
       "/deepscienceObject": {
         target:
           "https://ssproxy.ucloudbiz.olleh.com/v1/AUTH_2197aef3-436e-443d-a060-df0b98a86913",
         changeOrigin: true,
         secure: false,
+        rewrite: (path) => path.replace(/^\/deepscienceObject/, ""),
       },
     },
   },
